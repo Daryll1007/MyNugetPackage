7e896ac599985aab2df73233d24699f0215233b9
Daryll1007 <your_email@example.com>
2024-12-13 23:54:03 -0500

    Remove .vs folder from Git tracking

---
+++ MyNugetPackage/MyNugetPackage/MathOperations.cs
@@ -0,0 +1,33 @@
+﻿namespace MyNugetPackage
+{
+    public class MathOperations
+    {
+        // Add two numbers
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Subtract two numbers
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        // Multiply two numbers
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        // Divide two numbers
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Division by zero is not allowed.");
+            }
+            return a / b;
+        }
+    }
+}
